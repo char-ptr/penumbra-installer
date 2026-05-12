04091af9c0a190089800e7eda284b363df652077
amelia <ame@waow.ong>
2026-05-12 18:47:26 +0100

    linux support yaywoo!

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
+#[cfg(windows)]
 extern crate windres;
+#[cfg(windows)]
 use windres::Build;
 fn main() {
+    #[cfg(windows)]
     Build::new().compile("pi.rc").unwrap()
 }
-
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::path::PathBuf;
 
 use anyhow::Ok;
 use clap::Parser;
-use native_dialog::{MessageDialog, MessageType};
 use path_absolutize::Absolutize;
-
+use xdialog::{
+    show_message, show_message_error_ok, show_message_info_ok, show_message_yes_no, XDialogBuilder,
+};
+static ASSETS_DIR: include_dir::Dir = include_dir::include_dir!("./assets");
 #[derive(clap::Parser, Debug)]
 struct Args {
     #[arg(short, long)]
@@ -13,6 +15,53 @@ struct Args {
     install_ext: bool,
 }
 
+#[cfg(not(windows))]
+fn install_exts() -> anyhow::Result<()> {
+    use std::{env::current_exe, fs, io::Write, path::Path};
+
+    use anyhow::anyhow;
+    use directories::ProjectDirs;
+
+    let base_dirs = directories::BaseDirs::new().ok_or(anyhow!("failed to get user dirs"))?;
+    let project_dirs = ProjectDirs::from("ong", "waow", "penumbra-installer")
+        .ok_or(anyhow!("failed to get project dir"))?;
+    let data_dir = project_dirs.data_local_dir();
+    println!("data dir at {data_dir:?}");
+
+    fs::create_dir_all(data_dir)?;
+    ASSETS_DIR.extract(data_dir)?;
+    let this_program = current_exe()?;
+
+    let sym_linked = data_dir.join("penumbra-installer");
+    std::os::unix::fs::symlink(this_program, sym_linked)?;
+
+    let desktop_file_str = r#"[Desktop Entry]
+Name=Penumbra Installer
+Comment=Penumbra installer
+Icon=$STORE/pen.png
+Path=$STORE
+Exec=$STORE/penumbra-installer -m %u
+Terminal=false
+Type=Application
+StartupNotify=true
+StartupWMClass=penumbra-installer
+MimeType=application/zip
+        "#
+    .replace(
+        "$STORE",
+        data_dir.to_str().ok_or(anyhow!("unable to get data_dir"))?,
+    );
+    let desktop_path = data_dir.join("penumbra-installer.desktop");
+    let mut desktop_file = fs::File::create_new(&desktop_path)?;
+    desktop_file.write_all(desktop_file_str.as_bytes())?;
+    let to_symlink = base_dirs
+        .data_local_dir()
+        .join("applications/penumbra-installer.desktop");
+
+    std::os::unix::fs::symlink(desktop_path, to_symlink)?;
+
+    Ok(())
+}
 #[cfg(windows)]
 fn install_exts() -> anyhow::Result<()> {
     use winreg::{enums::HKEY_CLASSES_ROOT, RegKey};
@@ -40,31 +89,49 @@ fn install_exts() -> anyhow::Result<()> {
     tt2.0.set_value("", &"penumbra-installer")?;
     Ok(())
 }
+fn logic() -> anyhow::Result<()> {
+    let confirmation = show_message_yes_no(
+        "https://github.com/pozm/penumbra-installer - file ext installer",
+        " install .desktop or extensions in registry",
+        "Would you like to register the file extensions?",
+        xdialog::XDialogIcon::Information,
+    )?;
+    if !confirmation {
+        return Ok(());
+    }
+    let install_res = install_exts();
+    println!("install outcome = {install_res:?}");
+    if let Err(res) = install_res {
+        println!("failed to install : {:?}", res);
+        println!("if u want to re run on linux delete .desktop at ~/.local/share/applications/penumbra-installer.desktop and ~/.local/share/penumbra-installer");
+        show_message_error_ok(
+            "https://github.com/pozm/penumbra-installer - Unable to install file ext",
+            "failed to install :c",
+            "if ur on windows try run as admin",
+        );
+        // return Ok(xdialog::MessageDialog::new()
+        //     .set_type(xdialog::MessageType::Info)
+        //     .set_title("https://github.com/pozm/penumbra-installer - Unable to install file ext")
+        //     .set_text("To register this program as the default program for .ttmp2, .ttmp and .pmp files, please rerun this program as an admin.")
+        //     .show_alert()?);
+    } else {
+        show_message_yes_no(
+            "Successfully Installed",
+            "Successfully Installed",
+            "successfully installed - close to exit",
+            xdialog::XDialogIcon::Information,
+        )?;
+    }
+
+    Ok(())
+}
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     if args.install_ext {
         install_exts().expect("unable to install file exts")
     } else if args.mod_path.is_none() {
-        let confirmation = MessageDialog::new()
-            .set_type(MessageType::Info)
-            .set_title("https://github.com/pozm/penumbra-installer - file ext installer")
-            .set_text("Would you like to register the file extensions?")
-            .show_confirm()?;
-        if !confirmation {
-            return Ok(());
-        }
-        let install_res = install_exts();
-        if let Err(res) = install_res {
-            println!("failed to install : {:?}", res);
-            return Ok(MessageDialog::new().set_type(MessageType::Info).set_title("https://github.com/pozm/penumbra-installer - Unable to install file ext").set_text("To register this program as the default program for .ttmp2, .ttmp and .pmp files, please rerun this program as an admin.").show_alert()?);
-        } else {
-            MessageDialog::new()
-                .set_type(MessageType::Info)
-                .set_title("Successfully installed.")
-                .set_text("successfully installed - close to exit")
-                .show_alert()?;
-        }
+        return XDialogBuilder::new().run_result(logic);
     }
 
     let Some(path) = args.mod_path else {
